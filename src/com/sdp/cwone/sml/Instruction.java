89000c461f4aa4d7876207e7a49ee19b6c5b6ff5
BasilMason <basilmason@hotmail.com>
2016-01-24 18:17:04 +0000

    Checks for whitespace in label and valid sml codes

--- src/com/sdp/cwone/sml/Instruction.java
+++ src/com/sdp/cwone/sml/Instruction.java
@@ -1,5 +1,7 @@
 package com.sdp.cwone.sml;
 
+import java.util.Arrays;
+
 /**
  * This class is the superclass of the classes for machine instructions
  *
@@ -7,13 +9,29 @@ package com.sdp.cwone.sml;
  */
 
 public abstract class Instruction {
+
+    protected final String[] language = {"add", "sub", "mul", "div", "out", "lin", "bnz"};
+
     protected String label;
     protected String opcode;
 
     // Constructor: an instruction with label l and opcode op
     // (op must be an operation of the language)
-
     public Instruction(String l, String op) {
+
+        // check for whitespace
+        for (char c : l.toCharArray()) {
+
+            if (Character.isWhitespace(c))
+                throw new RuntimeException(l + " is not a valid label.");
+
+        }
+
+        // check valid SML opcode
+        if (!Arrays.asList(language).contains(op))
+            throw new RuntimeException(op + " is not a valid SML operation.");
+
+        // set label and opcode
         this.label = l;
         this.opcode = op;
     }
